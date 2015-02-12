c5134305f68734557984762f3f23d293ae0c54f1
Wasin <wazzinw@gmail.com>
2015-02-12 17:52:28 +0700

    API update

--- server/api.js
+++ server/api.js
@@ -9,19 +9,14 @@ HTTP.publish({collection: Customers}, function(data){
 	return Customers.find({});
 });
 
-HTTP.publish({collection: Menus}, function(data){
-	return Menus.find({});
-});
+
 
 HTTP.publish({collection: Order}, function(data){
 	return Order.find({});
 });
 
-HTTP.publish({collection: Staff}, function(data){
-	return Staff.find({});
-});
 
-HTTP.publish({collection: Restaurant}, function(data){
+/*HTTP.publish({collection: Restaurant}, function(data){
 	return Restaurant.find({});
-});
+});*/
 
