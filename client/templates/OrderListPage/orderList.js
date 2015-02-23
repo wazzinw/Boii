0a9b4ea0e4a92ecb7750b41cff010204c2abce0a
Wasin <wazzinw@gmail.com>
2015-02-23 15:12:48 +0700

    Updated API
    Restaurant and Menu with type

--- client/templates/OrderListPage/orderList.js
+++ client/templates/OrderListPage/orderList.js
@@ -3,10 +3,13 @@
  */
 
 //console.log('order');
-
-
-
 //find Menu in the restaurant
-//restaurant_menu = Restaurants.find({_id: { $in : Restaurants.menu } } ).toArray() ;
+    var menu_id = Restaurants.find({name: 'McDonald'}, {fields: {_id: 0, name: 0 ,
+        beaconID: 0, email: 0 , address: 0, phone: 0}}).fetch();
 
+    //id_array = menu_id[0].menu;
+
+    //restaurant_menu = Menus.find({_id: { $in : menu_id[0].menu} } ).fetch() ;
+
+    //console.log(restaurant_menu.length);
 
