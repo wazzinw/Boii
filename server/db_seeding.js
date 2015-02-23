6db2b19bb8c7b69810f7187e803cbb213be31d28
Wasin <wazzinw@gmail.com>
2015-02-23 22:21:34 +0700

    Updated restaurant and manu API
    Change data type in restaurant

--- server/db_seeding.js
+++ server/db_seeding.js
@@ -62,11 +62,11 @@ if(Restaurants.find().count() == 0) {
 
 /*_id: ,
 name: ,
-pic: ,
+pic_url: ,
 promotion: ,
 valid_until: ,
 price: ,
-categ: ,
+type: ,
 */
 
 if(Menus.find().count() == 0){
@@ -74,24 +74,24 @@ if(Menus.find().count() == 0){
        data =  [{
             //_id: ObjectID('AAAA'),
             name: 'McChicken',
-            pic: '',
-            promotion: 'true',
-            valid_until: new Date(2015,2,3),
-            price: '20' ,
-            categ: 'food',
-            restName: 'McDonald'
+            pic_url: null,
+            promotion: true,
+            valid_until: new Date(2015,2,3).toJSON(),
+            price: 20 ,
+            type: 'food',
+            restaurant_name: 'McDonald'
 
         },
 
         {
            // _id: ObjectID('BBBB'),
             name: 'McFish',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '20' ,
-            categ: 'food',
-            restName: 'McDonald'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 20 ,
+            type: 'food',
+            restaurant_name: 'McDonald'
 
 
         },
@@ -99,24 +99,24 @@ if(Menus.find().count() == 0){
         {
             //_id: ObjectID('AAAA'),
             name: 'McHappy',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '100' ,
-            categ: 'food',
-            restName: 'McDonald'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 100 ,
+            type: 'food',
+            restaurant_name: 'McDonald'
 
         },
 
         {
             // _id: ObjectID('BBBB'),
             name: 'Est',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '60' ,
-            categ: 'drink',
-            restName: 'McDonald'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 60 ,
+            type: 'drink',
+            restaurant_name: 'McDonald'
 
 
         },
@@ -124,12 +124,12 @@ if(Menus.find().count() == 0){
         {
             // _id: ObjectID('BBBB'),
             name: 'Roasted Duck',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '200' ,
-            categ: 'drink',
-            restName: 'MK'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 200 ,
+            type: 'drink',
+            restaurant_name: 'MK'
 
 
         },
@@ -137,24 +137,24 @@ if(Menus.find().count() == 0){
         {
             //_id: ObjectID('AAAA'),
             name: 'Dim Sum',
-            pic: '',
-            promotion: 'true',
-            valid_until: '02/08/2015',
-            price: '100' ,
-            categ: 'food',
-            restName: 'MK'
+            pic_url: null,
+            promotion: true,
+            valid_until: new Date(2015,2,3).toJSON(),
+            price: 100 ,
+            type: 'food',
+            restaurant_name: 'MK'
 
         },
 
         {
             // _id: ObjectID('BBBB'),
             name: 'Watermelon Juice',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '20' ,
-            categ: 'drink',
-            restName: 'MK'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 20 ,
+            type: 'drink',
+            restaurant_name: 'MK'
 
 
         },
@@ -162,12 +162,12 @@ if(Menus.find().count() == 0){
         {
             // _id: ObjectID('BBBB'),
             name: 'Apple Juice',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '20' ,
-            categ: 'drink',
-            restName: 'MK'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 20 ,
+            type: 'drink',
+            restaurant_name: 'MK'
 
 
         },
@@ -175,12 +175,12 @@ if(Menus.find().count() == 0){
         {
             // _id: ObjectID('BBBB'),
             name: 'Chicky',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '200' ,
-            categ: 'drink',
-            restName: 'KFC'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 200 ,
+            type: 'drink',
+            restaurant_name: 'KFC'
 
 
         },
@@ -188,24 +188,24 @@ if(Menus.find().count() == 0){
         {
             //_id: ObjectID('AAAA'),
             name: 'Ice Cream',
-            pic: '',
-            promotion: 'true',
-            valid_until: '02/08/2015',
-            price: '10' ,
-            categ: 'food',
-            restName: 'KFC'
+            pic_url: null,
+            promotion: true,
+            valid_until: new Date(2015,2,3).toJSON(),
+            price: 10 ,
+            type: 'food',
+            restaurant_name: 'KFC'
 
         },
 
         {
             // _id: ObjectID('BBBB'),
             name: 'Sprite',
-            pic: '',
-            promotion: 'false',
-            valid_until: '',
-            price: '30' ,
-            categ: 'drink',
-            restName: 'KFC'
+            pic_url: null,
+            promotion: false,
+            valid_until: null,
+            price: 30 ,
+            type: 'drink',
+            restaurant_name: 'KFC'
 
 
         },
@@ -213,12 +213,12 @@ if(Menus.find().count() == 0){
         {
             // _id: ObjectID('BBBB'),
             name: 'Milo',
-            pic: '',
-            promotion: 'false',
+            pic_url: null,
+            promotion: false,
             valid_until: '',
-            price: '30' ,
-            categ: 'drink',
-            restName: 'KFC'
+            price: 30 ,
+            type: 'drink',
+            restaurant_name: 'KFC'
 
 
         }];
@@ -235,18 +235,18 @@ if(Menus.find().count() == 0){
 
 
 Restaurants.update({name: 'MK'},
-    {$addToSet: { menu : {$each: Menus.find({restName: 'MK'},{fields: {name: 0, pic: 0, promotion: 0,
-                                valid_until: 0, price: 0, categ: 0, restName: 0}}).fetch()} } }
+    {$addToSet: { menu : {$each: Menus.find({restaurant_name: 'MK'},{fields: {name: 0, pic_url: 0, promotion: 0,
+                                valid_until: 0, price: 0, type: 0, restaurant_name: 0}}).fetch()} } }
 );
 
 Restaurants.update({name: 'McDonald'},
-    {$addToSet: { menu : {$each: Menus.find({restName: 'McDonald'}, {fields: {name: 0, pic: 0, promotion: 0,
-        valid_until: 0, price: 0, categ: 0, restName: 0}}).fetch()} } }
+    {$addToSet: { menu : {$each: Menus.find({restaurant_name: 'McDonald'}, {fields: {name: 0, pic_url: 0, promotion: 0,
+        valid_until: 0, price: 0, type: 0, restaurant_name: 0}}).fetch()} } }
 );
 
 Restaurants.update({name: 'KFC'},
-    {$addToSet: { menu : {$each: Menus.find({restName: 'KFC'}, {fields: {name: 0, pic: 0, promotion: 0,
-        valid_until: 0, price: 0, categ: 0, restName: 0}}).fetch()} } }
+    {$addToSet: { menu : {$each: Menus.find({restaurant_name: 'KFC'}, {fields: {name: 0, pic_url: 0, promotion: 0,
+        valid_until: 0, price: 0, type: 0, restaurant_name: 0}}).fetch()} } }
 );
 
 //Order
