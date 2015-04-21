6c0aeaf0e6636ee27f6b69c2706fe29fcafbcd95
Harin Sanghirun <harin.sanghirun@gmail.com>
2015-04-21 15:34:26 +0700

    fix apn for deployment

--- server/apn.js
+++ server/apn.js
@@ -3,33 +3,39 @@ Meteor.methods({
 	'pushOrderUpdate': function(id){
 	var apn = Meteor.npmRequire('apn')
 		var path = Meteor.npmRequire('path')
-		console.log(process.cwd())
-		var appRootPath = "../../../../../"
+		console.log("path="+process.cwd());
+		var appRootPath = "assets/app/"
 		var options = {
-			cert: appRootPath + "private/boii_aps_cer.pem",
-			key: appRootPath + "private/boii_aps_key.pem",
+			cert: appRootPath + "boii_aps_cer.pem",
+			key: appRootPath +  "boii_aps_key.pem",
 			passphrase: "boiisusu",
 			production: false
 		}
 		var apnConnection = new apn.Connection(options)
 
 		//Get user of current order
-		var order = Orders.findOne({_id: id});
-		if (order) {
-			//Set deviceToken
-			var user = Meteor.users.findOne({_id: order.customer_id});
+		try {
 
-			if (user) {
-				console.log("Sending push to deviceToken="+user.profile.deviceToken+ " with status="+order.order_status);
-				var myDevice = new apn.Device(user.profile.deviceToken);		
-				//Send push notification
-				var note = new apn.Notification();
-				note.sound = "default";
-				note.alert = "Order " + order.confirm_code + " has been " + order.order_status ;
-				note.payload = {'order_id': order._id, 'order_status': order.order_status};
+			var order = Orders.findOne({_id: id});
+			if (order) {
+				//Set deviceToken
+				var user = Meteor.users.findOne({_id: order.customer_id});
 
-				apnConnection.pushNotification(note, myDevice);
+				if (user) {
+					console.log("Sending push to deviceToken="+user.profile.deviceToken+ " with status="+order.order_status);
+					var myDevice = new apn.Device(user.profile.deviceToken);		
+					//Send push notification
+					var note = new apn.Notification();
+					note.sound = "default";
+					note.alert = "Order " + order.confirm_code + " has been " + order.order_status ;
+					note.payload = {'order_id': order._id, 'order_status': order.order_status};
+
+					apnConnection.pushNotification(note, myDevice);
+				}
 			}
+		} catch (e) {
+			console.log(e);
 		}
+		
 	}
 });
